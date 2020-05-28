df20006d29ba670774d041229c640b92e159b0c5
kurumaebi <knight7446@gmail.com>
2020-05-28 23:44:44 +0900

    11.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save{self.email = email.downcase}
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, 
                     length:{ maximum:50 }
   validates :email, presence: true, 
@@ -42,4 +43,12 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  def downcase_email
+    self.email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end

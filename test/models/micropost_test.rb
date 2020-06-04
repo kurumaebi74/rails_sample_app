ab29205a8c82fcaa8f4f86eb79141705147eb130
kurumaebi <knight7446@gmail.com>
2020-06-04 22:46:41 +0900

    create micropost controller

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    @micropost = @user.microposts.new(content:"Lorem ipsum")
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -24,5 +24,9 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be most 140 characters" do
     @micropost.content = "s"*141
     assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 end

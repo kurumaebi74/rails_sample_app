8508f53ab747be3e545f37ba1b6549d265045978
kurumaebi <knight7446@gmail.com>
2020-06-07 00:45:50 +0900

    13.3.3

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,23 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user,only: [:destroy,:create]
+
+  def create
+    @micropost = current_user.microposts.build(micopost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_path
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def micopost_params
+      params.require(:micropost).permit(:content)
+    end
 end

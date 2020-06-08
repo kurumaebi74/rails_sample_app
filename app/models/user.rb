965770bd5e2d384fae382ba20bd041a799fd0ae8
kurumaebi <knight7446@gmail.com>
2020-06-09 00:24:53 +0900

    create relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent:   :destroy
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save :downcase_email
   before_create :create_activation_digest
